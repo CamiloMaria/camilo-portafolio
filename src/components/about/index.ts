3a399418b9bfc448c156c32fa43c4e73b435ba94
cmaria08 <cmaria@plazalama.com>
2025-05-13 13:10:47 -0400

    Remove SkillBar and TimelineItem components from the About section, streamlining the component structure. Update index.ts to reflect these removals, enhancing clarity and organization within the codebase.

--- src/components/about/index.ts
+++ src/components/about/index.ts
@@ -1,5 +1,3 @@
-import SkillBar from "./skill-bar";
-import TimelineItem from "./time-line-item";
 import InterestItem from "./interest-item";
 import BioTab from "./bio-tab";
 import ExperienceCard from "./experience-card";
@@ -7,8 +5,6 @@ import SkillCard from "./skill-card";
 import ProfileCube from "./profile-cube";
 
 export {
-    SkillBar,
-    TimelineItem,
     InterestItem,
     BioTab,
     ExperienceCard,
